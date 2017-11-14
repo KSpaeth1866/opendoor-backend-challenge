a198f9e70bf51396f6be57222c48606e8e2ef269
Kobie <spaet062@umn.edu>
2017-11-14 13:18:06 -0800

    get route complete

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const axios = require('axios');
+const d3 = require('d3-dsv');
+const fs = require('fs');
 
 const routes = require('./routes')
 app.use('/', routes);
 
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`\n*******\n\nRunning on port ${port}\n\n*******\n`)
+app.listen(port, async () => {
+  try {
+    let obj = await axios.get('https://s3.amazonaws.com/opendoor-problems/listing-details.csv');
+    fs.writeFile('./data/listing-details.csv', obj.data, 'utf8', (error, resp) => {
+      if (error) console.log(`\n*******\n\nERROR in CSV PARSE:\n${error}\n\n*******\n`);
+    });
+    console.log(`\n*******\n\nRunning on port ${port}\n\n*******\n`)
+  }
+  catch (error) {
+    console.log(`\n*******\n\nERROR:\n${error}\n\n*******\n`);
+  }
 })
